f75355e2278548d436c6aadf7ce2ef01afb29b90
jesus.higueras <jesus.higueras@bbva.com>
2018-11-15 14:39:27 +0100

    fixed deployment

--- deploy.sh
+++ deploy.sh
@@ -4,27 +4,25 @@ set -o pipefail
 set -o allexport
 [[ "${DEBUG}" == 'true' ]] && set -o xtrace
 
-if [[ $# -gt 9 || $# -lt 6 ]]; then
-    echo "Usage: ./deploy.sh <AWS_ACCESS_KEY> <AWS_SECRET_KEY> <AWS_DEFAULT_REGION> <AWS_DOCKER_REGISTRY> <POPULATION> <COMPANIES> [<NUMBER_OF_TICKS>]"
-    echo "Usage: ./deploy.sh <AWS_ACCESS_KEY> <AWS_SECRET_KEY> <AWS_DEFAULT_REGION> <AWS_DOCKER_REGISTRY> <POPULATION> <COMPANIES> [<STREAM_SERVICE_API_KEY> <STREAM_SERVICE_NAMESPACE>] [<NUMBER_OF_TICKS>]"
+if [[ $# -gt 7 || $# -lt 4 ]]; then
+    echo "Usage: ./deploy.sh <AWS_DOCKER_REGISTRY> <POPULATION> <COMPANIES> <SEED_CAPITAL> [<NUMBER_OF_TICKS>]"
+    echo "Usage: ./deploy.sh <AWS_DOCKER_REGISTRY> <POPULATION> <COMPANIES> <SEED_CAPITAL> [<STREAM_SERVICE_API_KEY> <STREAM_SERVICE_NAMESPACE>] [<NUMBER_OF_TICKS>]"
     exit 1
 fi
-AWS_ACCESS_KEY="$1"
-AWS_SECRET_KEY="$2"
-AWS_DEFAULT_REGION="$3"
-AWS_DOCKER_REGISTRY="$4"
-POPULATION="$5"
-COMPANIES="$6"
+AWS_DOCKER_REGISTRY="$1"
+POPULATION="$2"
+COMPANIES="$3"
+SEED_CAPITAL="$4"
 
-if [[ $# -gt 7 ]]; then
-    STREAM_SERVICE_API_KEY="$7"
-    STREAM_SERVICE_NAMESPACE="$8"
+if [[ $# -gt 5 ]]; then
+    STREAM_SERVICE_API_KEY="$5"
+    STREAM_SERVICE_NAMESPACE="$6"
 fi
 
-if [[ $# -eq 7 ]]; then
+if [[ $# -eq 5 ]]; then
+    NUMBER_OF_TICKS="$5"
+elif [[ $# -eq 7 ]]; then
     NUMBER_OF_TICKS="$7"
-elif [[ $# -eq 9 ]]; then
-    NUMBER_OF_TICKS="$9"
 fi
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -33,12 +31,11 @@ source ${CURRENT_DIR}/deploy/aws-tools.sh
 loginDockerRegistry
 mvn docker:push -Ddocker.registry=${AWS_DOCKER_REGISTRY}
 
-configureAws
 AWS_LOGS_GROUP="boinet-container-logs"
 createCloudwatchLogsGroup ${AWS_LOGS_GROUP}
 
-CLUSTER_NAME="boinet-test"
-TASK_DEFINITION_NAME="boinet-test-task"
+CLUSTER_NAME="boinet"
+TASK_DEFINITION_NAME="boinet-task"
 TASK_DEFINITION_FILE="${CURRENT_DIR}/deploy/ecs-task-definition.json"
-createEcsTaskDefinition ${TASK_DEFINITION_NAME} ${TASK_DEFINITION_FILE} ${AWS_DOCKER_REGISTRY} ${AWS_LOGS_GROUP} ${AWS_DEFAULT_REGION} ${POPULATION} ${COMPANIES} ${STREAM_SERVICE_API_KEY} ${STREAM_SERVICE_NAMESPACE} ${NUMBER_OF_TICKS}
+createEcsTaskDefinition ${TASK_DEFINITION_NAME} ${TASK_DEFINITION_FILE} ${AWS_DOCKER_REGISTRY} ${AWS_LOGS_GROUP} ${AWS_DEFAULT_REGION} ${POPULATION} ${COMPANIES} ${SEED_CAPITAL} ${STREAM_SERVICE_API_KEY} ${STREAM_SERVICE_NAMESPACE} ${NUMBER_OF_TICKS}
 runEcsTask ${CLUSTER_NAME} ${TASK_DEFINITION_NAME}
--- deploy/aws-tools.sh
+++ deploy/aws-tools.sh
@@ -1,48 +1,33 @@
 #!/usr/bin/env bash
 
-awsCli() {
-    : ${AWS_ACCESS_KEY:?"AWS_ACCESS_KEY must be set"}
-    : ${AWS_SECRET_KEY:?"AWS_SECRET_KEY must be set"}
-    mkdir -p ~/.aws
-    # image: mikesir87/aws-cli:1.11.172
-    echo "docker run -i --rm -u $(id -u):$(id -g) -v ${PWD}:${PWD} -v $HOME/.aws:/tmp/.aws -e HOME=/tmp -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY} mikesir87/aws-cli@sha256:dc1dc4cc10a74739cce778a883eb2ed906c3113b7a39f19f93eceb6ca205682e aws"
+function awsCli() {
+    docker run \
+        --rm \
+        $(tty &>/dev/null && echo "-i") \
+        -u $(id -u):$(id -g) \
+        -e AWS_PROFILE \
+        -e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI \
+        -v ${PWD}:${PWD} \
+        -v ${HOME}/.aws:/.aws \
+        mesosphere/aws-cli@sha256:fb590357c2cf74e868cf110ede38daf4bdf0ebd1bdf36c21d256aa33ab22fa6e \
+        "$@"
 }
 
-ecsCli() {
-    : ${AWS_ACCESS_KEY:?"AWS_ACCESS_KEY must be set"}
-    : ${AWS_SECRET_KEY:?"AWS_SECRET_KEY must be set"}
-    mkdir -p ~/.ecs
-    echo "docker run -i --rm -u $(id -u):$(id -g) -v $HOME/.ecs:/tmp/.ecs -e HOME=/tmp mnuma/docker-ecs-cli"
-}
-
-configureAws() {
-    : ${AWS_ACCESS_KEY:?"AWS_ACCESS_KEY must be set"}
-    : ${AWS_SECRET_KEY:?"AWS_SECRET_KEY must be set"}
+function loginDockerRegistry() {
     : ${AWS_DEFAULT_REGION:?"AWS_DEFAULT_REGION must be set"}
 
-    $(awsCli) configure set aws_access_key_id ${AWS_ACCESS_KEY}
-    $(awsCli) configure set aws_secret_access_key ${AWS_SECRET_KEY}
-    $(awsCli) configure set default.region ${AWS_DEFAULT_REGION}
+    $(awsCli ecr get-login --region ${AWS_DEFAULT_REGION} --no-include-email)
 }
 
-loginDockerRegistry() {
-    : ${AWS_ACCESS_KEY:?"AWS_ACCESS_KEY must be set"}
-    : ${AWS_SECRET_KEY:?"AWS_SECRET_KEY must be set"}
-    : ${AWS_DEFAULT_REGION:?"AWS_DEFAULT_REGION must be set"}
-
-    # image: mikesir87/aws-cli:1.11.172
-    $($(awsCli) ecr get-login --region ${AWS_DEFAULT_REGION} --no-include-email)
-}
-
-createCloudwatchLogsGroup() {
+function createCloudwatchLogsGroup() {
     local groupName="$1"
 
-    $(awsCli) logs create-log-group \
+    awsCli logs create-log-group \
         --log-group-name ${groupName} \
         || true
 }
 
-createEcsTaskDefinition() {
+function createEcsTaskDefinition() {
     local family="$1"
     local taskDefinitionFile="$2"
     local awsDockerRegistry="$3"
@@ -50,14 +35,15 @@ createEcsTaskDefinition() {
     local awsRegion="$5"
     local population="$6"
     local companies="$7"
-    if [[ $# -gt 8 ]]; then
-        local streamServiceApiKey="$8"
-        local streamServiceNamespace="$9"
+    local seedCapital="$8"
+    if [[ $# -gt 9 ]]; then
+        local streamServiceApiKey="$9"
+        local streamServiceNamespace="${10}"
     fi
-    if [[ $# -eq 8 ]]; then
-        local numberOfTicks="$8"
-    elif [[ $# -eq 10 ]]; then
-        local numberOfTicks="${10}"
+    if [[ $# -eq 9 ]]; then
+        local numberOfTicks="$9"
+    elif [[ $# -eq 11 ]]; then
+        local numberOfTicks="${11}"
     fi
 
     sed -i.original "s~{{AWS_DOCKER_REGISTRY}}~${awsDockerRegistry}~" ${taskDefinitionFile}
@@ -65,13 +51,15 @@ createEcsTaskDefinition() {
     sed -i.original "s~{{AWSLOGS_REGION}}~${awsRegion}~" ${taskDefinitionFile}
     sed -i.original "s~{{STREAM_SERVICE_API_KEY}}~${streamServiceApiKey}~" ${taskDefinitionFile}
     sed -i.original "s~{{STREAM_SERVICE_NAMESPACE}}~${streamServiceNamespace}~" ${taskDefinitionFile}
+    sed -i.original "s~{{SEED_CAPITAL}}~${seedCapital}~" ${taskDefinitionFile}
     sed -i.original "s~{{POPULATION}}~${population}~" ${taskDefinitionFile}
     sed -i.original "s~{{COMPANIES}}~${companies}~" ${taskDefinitionFile}
     sed -i.original "s~{{NUMBER_OF_TICKS}}~${numberOfTicks}~" ${taskDefinitionFile}
     rm  ${taskDefinitionFile}.original
 
-    $(awsCli) ecs register-task-definition \
-        --family ${family} \
+    awsCli ecs register-task-definition                                             \
+        --family ${family}                                                          \
+        --execution-role-arn arn:aws:iam::809230366679:role/ecsTaskExecutionRole    \
         --cli-input-json file:///${taskDefinitionFile}
 
     sed -i.original "s~${awsDockerRegistry}~\{\{AWS_DOCKER_REGISTRY\}\}~" ${taskDefinitionFile}
@@ -79,18 +67,20 @@ createEcsTaskDefinition() {
     sed -i.original "s~${awsRegion}~\{\{AWSLOGS_REGION\}\}~" ${taskDefinitionFile}
     sed -i.original "s~--stream-service-api-key=${streamServiceApiKey}~--stream-service-api-key=\{\{STREAM_SERVICE_API_KEY\}\}~" ${taskDefinitionFile}
     sed -i.original "s~--stream-service-namespace=${streamServiceNamespace}~--stream-service-namespace=\{\{STREAM_SERVICE_NAMESPACE\}\}~" ${taskDefinitionFile}
-    sed -i.original "s~--population=${population}~--population=\{\{POPULATION\}\}~" ${taskDefinitionFile}
     sed -i.original "s~--companies=${companies}~--companies=\{\{COMPANIES\}\}~" ${taskDefinitionFile}
+    sed -i.original "s~--population=${population}~--population=\{\{POPULATION\}\}~" ${taskDefinitionFile}
+    sed -i.original "s~--seed-capital=${seedCapital}~--seed-capital=\{\{SEED_CAPITAL\}\}~" ${taskDefinitionFile}
     sed -i.original "s~--number-of-ticks=${numberOfTicks}~--number-of-ticks=\{\{NUMBER_OF_TICKS\}\}~" ${taskDefinitionFile}
     rm  ${taskDefinitionFile}.original
 }
 
-runEcsTask() {
+function runEcsTask() {
     local clusterName="$1"
     local taskDefinitionName="$2"
 
-    $(awsCli) ecs run-task \
-        --cluster ${clusterName} \
+    awsCli ecs run-task                         \
+        --cluster ${clusterName}                \
         --task-definition ${taskDefinitionName} \
-        --count 1
+        --count 1                               \
+        --launch-type FARGATE
 }
